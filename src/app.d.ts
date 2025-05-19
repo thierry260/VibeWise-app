97b2df1495dfa4cfbfa9d1881e8649bf847bf128
Thierry <thierry@gravitymedia.nl>
2025-05-19 11:25:50 +0200

    TypeScript type declarations for Svelte components
    When using Svelte 5 with TypeScript, you need the proper JSX type definitions to help TypeScript understand Svelte's HTML elements and attributes.

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,7 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+/// <reference types="svelte" />
+
 declare global {
 	namespace App {
 		// interface Error {}
@@ -7,6 +9,12 @@ declare global {
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
+	}
+
+	namespace svelte.JSX {
+		interface HTMLAttributes {
+			[key: string]: unknown;
+		}
 	}
 }
 
